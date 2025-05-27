70aece65bb3c1a75b1eea05ab80214af2fc370ec
nelson <chinwubanelson@gmail.com>
2025-05-27 19:08:36 +0100

    added desc

--- client/src/components/OurPortfolio.jsx
+++ client/src/components/OurPortfolio.jsx
@@ -16,12 +16,18 @@ const OurPortfolio = () => {
               src1={"/vicel_desk.png"}
               src2={"/vicel_mobile.png"}
               type={"Ecommerce web application"}
+              desc={
+                " FeatureS cart,order history, and admin panel, user authentication and authorization , image upload and product management."
+              }
               link={"https://vicel-pharmacy.onrender.com/"}
             />
 
             <PortfolioCard
               src1={"/kedi_desk.png"}
               src2={"/kedi_mobile.png"}
+              desc={
+                "High-converting responsive landing page, performance-optimized and SEO-friendly."
+              }
               type={"Landing page website"}
               link={"https://kedi-pharma.vercel.app/"}
             />
@@ -29,18 +35,27 @@ const OurPortfolio = () => {
               src1={"/pingme_desk.png"}
               src2={"/pingme_mobile.png"}
               type={"Live chat app"}
+              desc={
+                "Real-time chat with online indicator, secure authentication and authorization, message delivery tone and efficient backend for scalability."
+              }
               link={"https://pingme-chat-app.vercel.app/"}
             />
             <PortfolioCard
               src1={"/nelpin_desk.png"}
               src2={"/nelpin_mobile.png"}
               type={"Pinterest Clone"}
+              desc={
+                "Pinterest-style UI with dynamic masonry layout, image upload with core social media features like save, follow/unfollow user, comment and like/unlike functionalities."
+              }
               link={"https://nelpinterest-app.vercel.app/"}
             />
             <PortfolioCard
               src1={"/lms_desktop.png"}
               src2={"/lms_mobile.png"}
               type={"Learning management system "}
+              desc={
+                "Intuitive learning UI design with AI powered chatbot,modern frontend architecture for scalability."
+              }
               link={"https://neldemy-lms.vercel.app/"}
             />
 
@@ -48,6 +63,9 @@ const OurPortfolio = () => {
               src1={"/mern_desk.png"}
               src2={"/mern_mobile.png"}
               type={"Authentication system"}
+              desc={
+                "Complete authentication system featuring login,logout, OTP-based email verification and password reset using google SMTP."
+              }
               link={"https://mernauth-frontend.vercel.app/"}
             />
           </div>
--- client/src/components/PortfolioCard.jsx
+++ client/src/components/PortfolioCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-const PortfolioCard = ({ type, link, src1, src2 }) => {
+const PortfolioCard = ({ type, desc, link, src1, src2 }) => {
   return (
     <div className=" w-full h-full flex flex-col md:flex-row justify-between items-center gap-4 relative my-4">
       <div className="flex-[2]">
         <div className="w-full h-full">
           <img src={src1} alt="" className="w-full h-full object-cover" />
         </div>
-        <div className="w-[20%] h-[100%] absolute bottom-0 left-0">
+        <div className="w-[20%] h-[100%] absolute bottom-16 md:bottom-0 left-0">
           <img src={src2} alt="" className="w-full h-full object-contain" />
         </div>
       </div>
-      <div className="flex-1  ">
+      <div className="flex-1 mt-0 md:mt-0 ">
         <h1 className="text-xl text-blue-900 font-semibold text-start ">
           {type}
         </h1>
-
+        <p className="py-2">{desc}</p>
         <a
           href={link}
           target="_blank"

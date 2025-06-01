a8c399a97fff39c1470e6a14d60e2611ec5f9a75
nelson <chinwubanelson@gmail.com>
2025-06-02 00:39:51 +0100

    added real estate

--- client/src/components/OurPortfolio.jsx
+++ client/src/components/OurPortfolio.jsx
@@ -2,6 +2,91 @@ import React from "react";
 import PortfolioCard from "./PortfolioCard";
 
 const OurPortfolio = () => {
+  const portfolioItems = [
+    {
+      src1: "/realdesk.png",
+      src2: "/realmobile.png",
+      type: "Real Estate web application",
+      desc: "This is a real estate web application that connects property owners to buyers through a multi-agent system. Features include an admin panel, user authentication and authorization, property listing and editing.",
+      link: "https://luxlife.onrender.com",
+      stack: [
+        "React.js",
+        "Tailwind",
+        "Node",
+        "Express",
+        "MongoDB",
+        "Cloudinary",
+      ],
+    },
+    {
+      src1: "/vicel_desk.png",
+      src2: "/vicel_mobile.png",
+      type: "Ecommerce web application",
+      desc: "This application automates pharmaceutical product sales, including cart, order history, user profile, payment gateway, and admin panel for image upload and product management.",
+      link: "https://vicel-pharmacy.onrender.com/",
+      stack: [
+        "React.js",
+        "Tailwind",
+        "Node",
+        "Express",
+        "Stripe",
+        "MongoDB",
+        "Cloudinary",
+      ],
+    },
+    {
+      src1: "/pingme_desk.png",
+      src2: "/pingme_mobile.png",
+      type: "Live chat app",
+      desc: "Real-time chat application for secure communication, featuring online indicator, secure authentication, and efficient backend for scalability.",
+      link: "https://pingme-chat-app.vercel.app/",
+      stack: [
+        "React.js",
+        "Tailwind",
+        "Daisy UI",
+        "Node",
+        "Express",
+        "Socket.io",
+        "Gemini",
+        "MongoDB",
+        "Cloudinary",
+      ],
+    },
+    {
+      src1: "/mern_desk.png",
+      src2: "/mern_mobile.png",
+      type: "Secure Authentication Sytem",
+      desc: "Secure authentication system featuring login,logout, OTP-based email verification and password reset using google SMTP. Highly scalable with robust backend",
+      link: "https://mernauth-frontend.vercel.app/",
+      stack: [
+        "React.js",
+        "Tailwind",
+        "Node",
+        "Express",
+        "Google SMTP",
+        "MongoDB",
+      ],
+    },
+
+    {
+      src1: "/lms_desktop.png",
+      src2: "/lms_mobile.png",
+      type: "Learning Management System",
+      desc: "Intuitive learning UI design with AI powered chatbot,modern frontend architecture for scalability. This is a well structured platform for learning and skill acquisition through tutorials and automated AI chatbot",
+      link: "https://neldemy-lms.vercel.app/",
+      stack: ["React.js", "Tailwind", " Gemini", "Node", "Express", "MongoDB"],
+    },
+
+    {
+      src1: "/kedi_desk.png",
+      src2: "/kedi_mobile.png",
+      type: "Landing page website",
+      desc: "High-converting responsive landing page, performance-optimized and SEO-friendly.",
+      link: "https://kedi-pharma.vercel.app/",
+      stack: ["React.js", "Tailwind"],
+    },
+  ];
+
   return (
     <section className="w-full h-full mt-5 pb-10 " id="portfolio">
       <div className="container">
@@ -12,62 +97,9 @@ const OurPortfolio = () => {
           {/* Portfolio */}
           {/* mobile version */}
           <div className="">
-            <PortfolioCard
-              src1={"/vicel_desk.png"}
-              src2={"/vicel_mobile.png"}
-              type={"Ecommerce web application"}
-              desc={
-                " Features cart,order history, and admin panel, user authentication and authorization , image upload and product management."
-              }
-              link={"https://vicel-pharmacy.onrender.com/"}
-            />
-
-            <PortfolioCard
-              src1={"/kedi_desk.png"}
-              src2={"/kedi_mobile.png"}
-              desc={
-                "High-converting responsive landing page, performance-optimized and SEO-friendly."
-              }
-              type={"Landing page website"}
-              link={"https://kedi-pharma.vercel.app/"}
-            />
-            <PortfolioCard
-              src1={"/pingme_desk.png"}
-              src2={"/pingme_mobile.png"}
-              type={"Live chat app"}
-              desc={
-                "Real-time chat with online indicator, secure authentication and authorization, message delivery tone and efficient backend for scalability."
-              }
-              link={"https://pingme-chat-app.vercel.app/"}
-            />
-            <PortfolioCard
-              src1={"/nelpin_desk.png"}
-              src2={"/nelpin_mobile.png"}
-              type={"Pinterest Clone"}
-              desc={
-                "Pinterest-style UI with dynamic masonry layout, image upload with core social media features like save, follow/unfollow user, comment and like/unlike functionalities."
-              }
-              link={"https://nelpinterest-app.vercel.app/"}
-            />
-            <PortfolioCard
-              src1={"/lms_desktop.png"}
-              src2={"/lms_mobile.png"}
-              type={"Learning management system "}
-              desc={
-                "Intuitive learning UI design with AI powered chatbot,modern frontend architecture for scalability."
-              }
-              link={"https://neldemy-lms.vercel.app/"}
-            />
-
-            <PortfolioCard
-              src1={"/mern_desk.png"}
-              src2={"/mern_mobile.png"}
-              type={"Authentication system"}
-              desc={
-                "Complete authentication system featuring login,logout, OTP-based email verification and password reset using google SMTP."
-              }
-              link={"https://mernauth-frontend.vercel.app/"}
-            />
+            {portfolioItems.map((item, index) => (
+              <PortfolioCard key={index} {...item} />
+            ))}
           </div>
         </div>
       </div>
--- client/src/components/PortfolioCard.jsx
+++ client/src/components/PortfolioCard.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 
-const PortfolioCard = ({ type, desc, link, src1, src2 }) => {
+const PortfolioCard = ({ type, desc, link, src1, src2, stack }) => {
   return (
     <div className=" w-full h-full flex flex-col md:flex-row justify-between items-center gap-4 relative my-4">
       <div className="flex-[2]">
         <div className="w-full h-full">
           <img src={src1} alt="" className="w-full h-full object-cover" />
         </div>
-        <div className="w-[20%] h-[100%] absolute bottom-16 md:bottom-0 left-0">
+        <div className="w-[20%] h-[100%] hidden md:block absolute bottom-12 md:bottom-0 left-0">
           <img src={src2} alt="" className="w-full h-full object-contain" />
         </div>
       </div>
-      <div className="flex-1 mt-0 md:mt-0 ">
+      <div className="flex-1  ">
         <h1 className="text-xl text-blue-900 font-semibold text-center ">
           {type}
         </h1>
         <p className="my-2 text-center">{desc}</p>
+        <div className="flex flex-col items-center">
+          <h3 className="text-orange-800 text-sm text-center">Stack</h3>
+          <div className="grid grid-cols-2">
+            {stack?.map((item, index) => (
+              <span key={index} className="px-1 text-xs  font-semibold">
+                {item}
+              </span>
+            ))}
+          </div>
+        </div>
         <a
           href={link}
           target="_blank"
